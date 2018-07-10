6a5f060ff73d17c0f3ffd6fea82f195308f057bc
kat2018 <kat.vge@gmail.com>
2018-07-10 15:39:02 -0400

    Add Age to Users Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_191842) do
+ActiveRecord::Schema.define(version: 2018_07_10_193736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_07_10_191842) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
   end
 
 end
